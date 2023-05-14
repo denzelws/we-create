a0ce2f72a718323f2f0b01f5ebdd138c1c429e57
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-05-14 02:39:11 -0300

    Add app structure and Navbar

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,13 @@
+import * as S from './styles'
+
+const Navbar = () => (
+  <S.Wrapper>
+    <S.Logo>we create</S.Logo>
+    <S.Portfolio>Portfolio</S.Portfolio>
+    <S.AboutUs>Sobre nós</S.AboutUs>
+    <S.Contact>Contato</S.Contact>
+    <button>Entre</button>
+  </S.Wrapper>
+)
+
+export default Navbar
---
+++ src/components/Navbar/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+display: flex;
+align-items: center;
+justify-content: space-between;
+padding: 3.5rem;
+background: #fafafa;
+color: black;
+`
+
+export const Logo = styled.h2`
+font-size: 1.8rem;
+`
+
+export const Portfolio = styled.h2``
+
+export const AboutUs = styled.h2``
+
+export const Contact = styled.h2``
